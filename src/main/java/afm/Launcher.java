69fa6d4d3f47634f53e75f3b0ef857aad1e109aa
koolskateguy89 <daraagbola@gmail.com>
2021-01-12 02:18:31 +0000

    Removed trailing whitepace

--- src/main/java/afm/Launcher.java
+++ src/main/java/afm/Launcher.java
@@ -3,13 +3,13 @@ package afm;
 /*
  * Wrapper around Main to allow Maven-shade-plugin to build
  * (Can't use Main as it extends Application)
- * 
+ *
  * Also means I don't have to use VM arguments to run it :)
  */
 class Launcher {
-	
+
 	public static void main(String[] args) {
 		Main.launch0(args);
 	}
-	
+
 }
