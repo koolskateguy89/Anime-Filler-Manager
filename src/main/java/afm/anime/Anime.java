081d565e8d48a2706ab891c5e30b3c744598da50
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2022-01-03 23:49:38 +0000

    :recycle: Switch Filler to Kotlin

--- src/main/java/afm/anime/Anime.java
+++ src/main/java/afm/anime/Anime.java
@@ -159,7 +159,7 @@ public final class Anime {
 		}
 
 		public AnimeBuilder addFillerAsString(String s) {
-			return addFiller(Filler.parseFiller(s));
+			return addFiller(Filler.valueOf(s));
 		}
 
 		public AnimeBuilder setInfo(String info) {
@@ -311,11 +311,11 @@ public final class Anime {
 			if (filler.contains(nextEp)) {
 				// 'skip' to the end of the filler range
 				// this is fine as consecutive filler SHOULD be in the same range
-				nextEp = filler.end() + 1;
+				nextEp = filler.getEnd() + 1;
 				break;
 			}
 
-			if (currEp < filler.start()) {
+			if (currEp < filler.getStart()) {
 				// if this if clause is triggered, we have gone past our episode,
 				// this should happen if the next episode is not filler
 				break;
