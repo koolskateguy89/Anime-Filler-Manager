c50859ae5c4a2e2154e74bd91b825c969f792d60
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2022-01-07 23:38:42 +0000

    :truck: Rename UtilityClasses.kt -> utilityClasses.kt

--- app/src/main/java/afm/common/utilityClasses.kt
+++ app/src/main/java/afm/common/utilityClasses.kt
@@ -9,7 +9,7 @@ package afm.common
  *
  * https://stackoverflow.com/a/42709643/17381629
  */
-open class NonNullMap<K, V>(private val map: Map<K, V>) : Map<K, V> by map {
+internal open class NonNullMap<K, V>(private val map: Map<K, V>) : Map<K, V> by map {
     override operator fun get(key: K): V {
         return map[key]!! // Force an NPE if the key doesn't exist
     }
