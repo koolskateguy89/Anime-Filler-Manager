ae5e5966c189d98f21d8e28a847aa78192534d3c
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2022-01-30 15:24:32 +0000

    :white_check_mark: [filler]: Add synonymsMap test (TODO)

---
+++ filler/src/test/kotlin/com/github/koolskateguy89/filler/SynonymsMapTest.kt
@@ -0,0 +1,12 @@
+package com.github.koolskateguy89.filler
+
+import kotlin.test.Test
+
+internal class SynonymsMapTest {
+
+    @Test
+    fun test() {
+        // TODO: test AnimeFillerList.fillerFor(String) using synonyms (after synonyms map is used)
+    }
+
+}
