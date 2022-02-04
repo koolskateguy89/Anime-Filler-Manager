f9ab11fbb162ffdd978dfd020049f2ca06cbcf7b
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2022-02-04 00:53:00 +0000

    :heavy_minus_sign: Remove Guava dep

--- app/src/main/java/afm/common/utils/CollectionUtils.kt
+++ app/src/main/java/afm/common/utils/CollectionUtils.kt
@@ -2,9 +2,7 @@ package afm.common.utils
 
 import java.util.EnumSet
 
-open class ImmutableEnumSet<E : Enum<E>> private constructor(
-    val backingSet: EnumSet<E>,
-) : Set<E> by backingSet {
+open class ImmutableEnumSet<E : Enum<E>> protected constructor(val backingSet: EnumSet<E>) : Set<E> by backingSet {
 
     @Deprecated("Always throws UnsupportedOperationException")
     fun add(v: E): Nothing {
@@ -17,17 +15,25 @@ open class ImmutableEnumSet<E : Enum<E>> private constructor(
 
     companion object {
         @JvmStatic
-        fun <E : Enum<E>> copyOf(values: Collection<E>): ImmutableEnumSet<E> = ImmutableEnumSet(EnumSet.copyOf(values))
+        fun <E : Enum<E>> viewOf(values: EnumSet<E>): ImmutableEnumSet<E> = ImmutableEnumSet(values)
 
         @JvmStatic
-        fun <E : Enum<E>> viewOf(values: EnumSet<E>): ImmutableEnumSet<E> = ImmutableEnumSet(values)
+        fun <E : Enum<E>> copyOf(values: EnumSet<E>): ImmutableEnumSet<E> = ImmutableEnumSet(values)
+
+        inline fun <reified E : Enum<E>> copyOf(values: Iterable<E>): ImmutableEnumSet<E> {
+            // use alternative constructor in case provided set is empty
+            return viewOf(EnumSet.noneOf(E::class.java).apply { addAll(values) })
+        }
+
+        inline fun <reified E: Enum<E>> build(init: EnumSet<E>.() -> Unit): ImmutableEnumSet<E> {
+            return viewOf(EnumSet.noneOf(E::class.java).apply(init))
+        }
     }
 }
 
-fun <E : Enum<E>> EnumSet<E>.immutable(): ImmutableEnumSet<E> = ImmutableEnumSet.copyOf(this)
+inline fun <reified E : Enum<E>> EnumSet<E>.immutable() = ImmutableEnumSet.copyOf(this)
 
-inline fun <reified E : Enum<E>> emptyEnumSet(): EnumSet<E> =
-    EnumSet.noneOf(E::class.java)
+inline fun <reified E : Enum<E>> emptyEnumSet(): EnumSet<E> = EnumSet.noneOf(E::class.java)
 
 fun <E> MutableCollection<E>.setAll(elements: Collection<E>): Boolean {
     clear()
