fe807857717c9c916d646f4f8796c7dc724c277c
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2022-05-29 15:59:10 +0100

    :alien: Update genres

--- app/src/main/java/afm/screens/infowindows/GenreInfoWindow.java
+++ app/src/main/java/afm/screens/infowindows/GenreInfoWindow.java
@@ -13,6 +13,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.TextArea;
 import javafx.scene.input.InputEvent;
 
@@ -23,191 +24,303 @@ import afm.screens.Menu;
 
 public class GenreInfoWindow extends InfoWindow {
 
-	// maybe this should be in Genre class
-	private static final Map<Genre, String> genreDefMap;
+    // maybe this should be in Genre class
+    private static final Map<Genre, String> genreDefMap;
+
+    static {
+        EnumMap<Genre, String> tempMap = new EnumMap<>(Genre.class);
+        // genre definitions: https://myanimelist.net/anime/genre/info
+        // TODO: not sure if all genres here
+
+        tempMap.put(Genre.Action, """
+        Plays out mainly through a clash of physical forces. Frequently these
+        stories have fast cuts, tough characters making quick decisions and
+        usually a beautiful girl nearby. Anything quick and most likely a thin
+        storyline.
+        """);
+
+        tempMap.put(Genre.Adventure, """
+        Travelling and undertaking an adventure in a certain place
+        """);
+
+        tempMap.put(Genre.AvantGarde, """
+        Anime that have mind-twisting plots.
+        """);
+
+        tempMap.put(Genre.AwardWinning, """
+        Won a major award.
+        """);
+
+        tempMap.put(Genre.BoysLove, """
+        Gay.
+        """);
+
+        tempMap.put(Genre.CGDCT, """
+        'Cute Girls Doing Cute Things'
+        """);
+
+        tempMap.put(Genre.Comedy, """
+        Aims to make you laugh
+        """);
+        // tempMap.put(Genre.Comedy, """
+        // Anime: *plays*
+        // You: *laughs*
+        // """);
+
+        tempMap.put(Genre.Detective, """
+        \uD83D\uDD75
+        """);
+
+        tempMap.put(Genre.Drama, """
+        Anime that often show life or characters through conflict and
+        emotions. In general, the different parts of the story tend to
+        form a whole that is greater than the sum of the parts. In other
+        words, the story has a message that is bigger than just the story
+        line itself.
+        """);
+
+        tempMap.put(Genre.Ecchi, """
+        Almost cartoon porn.
+        """);
+
+        tempMap.put(Genre.Erotica, """
+        \uD83D\uDE33
+        """);
+
+        tempMap.put(Genre.Fantasy, """
+        Based in a fanatical world.
+        """);
+
+        tempMap.put(Genre.GirlsLove, """
+        Lesbian.
+        """);
+
+        tempMap.put(Genre.Gourmet, """
+        Food.
+        """);
+
+        tempMap.put(Genre.Harem, """
+        Not many guys, but many, many girls.
+        """);
+
+        tempMap.put(Genre.Hentai, """
+        Cartoon porn.
+        """);
+
+        tempMap.put(Genre.Historical, """
+        Based on past historical events.
+        """);
+
+        tempMap.put(Genre.Horror, """
+        ooooo scarryyyy!
+        """);
+
+        tempMap.put(Genre.Josei, """
+        Aimed towards young adult women.
+        """);
+
+        tempMap.put(Genre.Kids, """
+        Aimed at kids
+        
+        
+        ...yikes
+        """);
+
+        tempMap.put(Genre.MagicalSexShift, """
+        One or more main characters in these stories identify as their biological sex,
+        but their body is suddenly changed to the opposite sex through magical means.
+        This Magical Sex Switch may be temporary, irreversible, or allow the character
+        to change back and forth between the two. Since the character's original
+        identity is not lost, coming to terms with the magical sex switch or finding a
+        method to change back should play a central role in the plot.
+        """);
 
-	static {
-		EnumMap<Genre, String> tempMap = new EnumMap<>(Genre.class);
-		// genre definitions: https://myanimelist.net/anime/genre/info
+        tempMap.put(Genre.MartialArts, """
+        MARTIAL ART.
+        """);
 
-		tempMap.put(Genre.Action, """
-								Plays out mainly through a clash of physical forces. Frequently these
-								stories have fast cuts, tough characters making quick decisions and
-								usually a beautiful girl nearby. Anything quick and most likely a thin
-								storyline.
-								""");
+        tempMap.put(Genre.Mecha, """
+        Involves robotic entities as a main feature.
+        """);
 
-		tempMap.put(Genre.Adventure, "Travelling and undertaking an adventure in a certain place");
+        tempMap.put(Genre.Military, """
+        An anime series/movie that has a heavy militaristic feel behind it.
+        """);
 
-		tempMap.put(Genre.AvantGarde, "Anime that have mind-twisting plots.");
+        tempMap.put(Genre.Music, """
+        Headphones and concerts etc.
+        """);
 
-		tempMap.put(Genre.AwardWinning, "Won a major award.");
+        tempMap.put(Genre.Mystery, """
+        ?
+        """);
 
-		tempMap.put(Genre.BoysLove, "Gay.");
+        tempMap.put(Genre.Mythology, """
+        e
+        """); // TODO
 
-		tempMap.put(Genre.Cars, "Involves cars as a main feature e.g. racing");
+        tempMap.put(Genre.Parody, "Naruto -> Narudo" + "\n" + "Spiderman -> Spooderman.");
 
-		tempMap.put(Genre.Comedy, "Aims to make you laugh");
-		//tempMap.put(Genre.Comedy, "Anime: *plays*" + "\n" + "You: *laughs*");
+        tempMap.put(Genre.Psychological, """
+        Brain.
+        """);
 
-		tempMap.put(Genre.Demons, "Involves demons as a main feature");
+        tempMap.put(Genre.Racing, """
+        VROOM VROOM!
+        """);
 
-		tempMap.put(Genre.Drama, """
-								Anime that often show life or characters through conflict and
-								emotions. In general, the different parts of the story tend to
-								form a whole that is greater than the sum of the parts. In other
-								words, the story has a message that is bigger than just the story
-								line itself.
-								""");
+        tempMap.put(Genre.Romance, """
+        Love.
+        """);
 
-		tempMap.put(Genre.Ecchi, "Almost cartoon porn.");
+        tempMap.put(Genre.Samurai, """
+        Sword.
+        """);
 
-		tempMap.put(Genre.Erotica, "\uD83D\uDE33");
+        tempMap.put(Genre.School, """
+        Based in a school setting.
+        """);
 
-		tempMap.put(Genre.Fantasy, "Based in a fanatical world.");
+        tempMap.put(Genre.SciFi, """
+        Science?
+        """);
 
-		tempMap.put(Genre.Game, """
-								Central theme is based on a non-violent, non-sports game, like
-								go, chess, trading card games or computer/video games.
-								""");
+        tempMap.put(Genre.Seinen, """
+        Aimed towards young adult men.
+        """);
 
-		tempMap.put(Genre.GirlsLove, "Lesbian.");
+        tempMap.put(Genre.Shoujo, """
+        Aimed at a "young female audience".
+        """);
 
-		tempMap.put(Genre.Gourmet, "Food.");
+        tempMap.put(Genre.Shounen, """
+        Aimed at a "young male audience".
+        """);
 
-		tempMap.put(Genre.Harem, "Not many guys, but many, many girls.");
+        tempMap.put(Genre.SliceOfLife, """
+        Aims to mimic real life through anime.
+        """);
 
-		tempMap.put(Genre.Hentai, "Cartoon porn.");
+        tempMap.put(Genre.Space, """
+        NASA.
+        """);
 
-		tempMap.put(Genre.Historical, "Based on past historical events.");
+        tempMap.put(Genre.Sports, """
+        Literally sports, or rather, physical exertion...
+        """);
 
-		tempMap.put(Genre.Horror, "ooooo scarryyyy.");
+        tempMap.put(Genre.StrategyGame, """
+        Use brain in game 2 win.
+        """);
 
-		tempMap.put(Genre.Josei, "Aimed towards young adult women.");
+        tempMap.put(Genre.SuperPower, """
+        Usually based in a word where having superpowers is the norm.
+        """);
 
-		tempMap.put(Genre.Kids, """
-								Aimed at kids
+        tempMap.put(Genre.Supernatural, """
+        strange.
+        """);
 
+        tempMap.put(Genre.Survival, """
+        """); // TODO
 
+        tempMap.put(Genre.Suspense, """
+        ...
+        """);
 
-								yikes
-								""");
+        tempMap.put(Genre.TeamSports, """
+        """); // TODO
 
-		tempMap.put(Genre.Magic, "Involves magic as a main feature.");
+        tempMap.put(Genre.TimeTravel, """
+        """); // TODO
 
-		tempMap.put(Genre.MartialArts, "MARTIAL ART.");
+        tempMap.put(Genre.Vampire, """
+        Draculaaaaa.
+        """);
 
-		tempMap.put(Genre.Mecha, "Involves robotic entities as a main feature.");
+        tempMap.put(Genre.VideoGame, """
+        """); // TODO
 
-		tempMap.put(Genre.Military, "An anime series/movie that has a heavy militaristic feel behind it.");
+        tempMap.put(Genre.VisualArts, """
+        """); // TODO
 
-		tempMap.put(Genre.Music, "Headphones and concerts etc.");
+        tempMap.put(Genre.Workplace, """
+        Working.
+        """);
 
-		tempMap.put(Genre.Mystery, "?");
+        genreDefMap = Collections.unmodifiableMap(tempMap);
+    }
 
-		tempMap.put(Genre.Parody, "Naruto -> Narudo" + "\n" + "Spiderman -> Spooderman.");
+    public static void open(@Nonnull Button infoBtn) {
+        Utils.setStyleClass(infoBtn, Menu.SELECTED);
 
-		tempMap.put(Genre.Police, "Based on characters who are involved in a police force.");
+        try {
+            new GenreInfoWindow(infoBtn).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-		tempMap.put(Genre.Psychological, "Brain.");
 
-		tempMap.put(Genre.Romance, "Love.");
+    private static final EventType<InputEvent> ALL_EVENTS = InputEvent.ANY;
+    private static final EventHandler<InputEvent> IGNORE_EVENT = InputEvent::consume;
 
-		tempMap.put(Genre.Samurai, "Sword.");
+    private final Button helpBtn;
 
-		tempMap.put(Genre.School, "Based in a school setting.");
+    @FXML
+    private ComboBox<Genre> genreCombo;
 
-		tempMap.put(Genre.SciFi, "Science?");
+    @FXML
+    private Hyperlink explanationLink;
 
-		tempMap.put(Genre.Seinen, "Aimed towards young adult men.");
+    @FXML
+    private TextArea textArea;
 
-		tempMap.put(Genre.Shoujo, "Aimed at a \"young female audience\".");
+    private GenreInfoWindow(Button helpBtn) throws IOException {
+        super(null);
+        this.helpBtn = helpBtn;
 
-		tempMap.put(Genre.Shounen, "Aimed at a \"young male audience\".");
+        // load FXML file into this object
+        FXMLLoader loader = new FXMLLoader(Utils.getFxmlUrl("infowindows/GenreInfoWindow"));
+        loader.setController(this);
+        loader.setRoot(this);
+        loader.load();
 
-		tempMap.put(Genre.SliceOfLife, "Aims to mimic real life through anime.");
+        // Stop main screen receiving input
+        Main.getStage().addEventFilter(ALL_EVENTS, IGNORE_EVENT);
+    }
 
-		tempMap.put(Genre.Space, "NASA.");
+    @FXML
+    void initialize() {
+        setTitle("Genre Explanations");
 
-		tempMap.put(Genre.Sports, "Literally sports, or rather, physical exertion...");
+        // add all Genres into the ComboBox
+        genreCombo.getItems().addAll(Genre.values());
 
-		tempMap.put(Genre.Supernatural, "strange.");
+        // set it so when user selects a genre, it sets textArea as the definiton
+        // of that Genre
+        genreCombo.valueProperty().addListener((obs, oldVal, newVal) -> {
+            if (newVal != null) {
+                textArea.setText(genreDefMap.get(newVal));
+            } else {
+                textArea.setText(null);
+            }
+        });
 
-		tempMap.put(Genre.SuperPower, "Usually based in a word where having superpowers is the norm.");
+        explanationLink.setOnAction(event ->
+                Main.getInstance().getHostServices().showDocument(explanationLink.getText()));
 
-		tempMap.put(Genre.Suspense, "...");
+        super.afterInitialize();
+    }
 
-		tempMap.put(Genre.Vampire, "Draculaaaaa.");
+    @Override @FXML
+    void closeWindow() {
+        helpBtn.getStyleClass().setAll("button");
+        helpBtn.setMouseTransparent(false);
 
-		tempMap.put(Genre.WorkLife, "About working.");
+        super.closeWindow();
 
-		genreDefMap = Collections.unmodifiableMap(tempMap);
-	}
-
-	public static void open(@Nonnull Button infoBtn) {
-		Utils.setStyleClass(infoBtn, Menu.SELECTED);
-
-		try {
-			new GenreInfoWindow(infoBtn).show();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-
-	private static final EventType<InputEvent> ALL_EVENTS = InputEvent.ANY;
-	private static final EventHandler<InputEvent> IGNORE_EVENT = InputEvent::consume;
-
-	private final Button helpBtn;
-
-	@FXML
-	private ComboBox<Genre> genreCombo;
-
-	@FXML
-	private TextArea textArea;
-
-	private GenreInfoWindow(Button helpBtn) throws IOException {
-		super(null);
-		this.helpBtn = helpBtn;
-
-		// load FXML file into this object
-		FXMLLoader loader = new FXMLLoader(Utils.getFxmlUrl("infowindows/GenreInfoWindow"));
-		loader.setController(this);
-		loader.setRoot(this);
-		loader.load();
-
-		// Stop main screen receiving input
-		Main.getStage().addEventFilter(ALL_EVENTS, IGNORE_EVENT);
-	}
-
-	@FXML
-	void initialize() {
-		setTitle("Genre Explanations");
-
-		// add all Genres into the ComboBox
-		genreCombo.getItems().addAll(Genre.values());
-
-		// set it so when user selects a genre, it sets textArea as the definiton
-		// of that Genre
-		genreCombo.valueProperty().addListener((obs, oldVal, newVal) -> {
-			if (newVal != null) {
-				textArea.setText(genreDefMap.get(newVal));
-			} else {
-				textArea.setText(null);
-			}
-		});
-
-		super.afterInitialize();
-	}
-
-	@Override @FXML
-	void closeWindow() {
-		helpBtn.getStyleClass().setAll("button");
-		helpBtn.setMouseTransparent(false);
-
-		super.closeWindow();
-
-		// allow main screen to receive input again
-		Main.getStage().removeEventFilter(ALL_EVENTS, IGNORE_EVENT);
-	}
+        // allow main screen to receive input again
+        Main.getStage().removeEventFilter(ALL_EVENTS, IGNORE_EVENT);
+    }
 }
--- core/src/main/java/afm/anime/Genre.kt
+++ core/src/main/java/afm/anime/Genre.kt
@@ -1,9 +1,50 @@
 package afm.anime
 
+import afm.common.utils.remove
 import afm.common.utils.splitByCapitals
+import org.jsoup.Jsoup
 
 enum class GenreType {
     NORMAL, DEMOGRAPHIC, THEME;
+}
+
+fun main() {
+    fun getGenres() {
+        val doc = Jsoup.connect("https://myanimelist.net/anime.php").get()
+        val pairs = doc.select(".anime-manga-search")
+            .first()!!
+            .children()
+            .take(8)
+            .chunked(2)
+
+        val result = pairs.map { (genreTypeE, genresE) ->
+            val genreType = genreTypeE.text()
+            // href="/anime/genre/1/Action"
+            val genres: List<Triple<Int, String, String>> = genresE.select(".genre-name-link").map {
+                val href = it.attr("href")
+                val lastSlash = href.lastIndexOf('/')
+
+                val genreId = href.substring(13, lastSlash).toInt()
+                val name = href.substring(lastSlash + 1).remove('_').remove('-')
+
+                Triple(genreId, name, genreType)
+            }
+            genres
+        }.flatten()
+
+        result.sortedBy { (id, name, major) ->
+            name
+        }.forEach { (id, name, major) ->
+            print("$name($id")
+            println(when (major) {
+                "Explicit Genres" -> ", explicit = true),"
+                "Themes" -> ", GenreType.THEME),  // Theme"
+                "Demographics" -> ", GenreType.DEMOGRAPHIC),  // Demographics"
+                else -> "),"
+            })
+        }
+    }
+    getGenres()
 }
 
 enum class Genre(val id: Int,
@@ -12,50 +53,81 @@ enum class Genre(val id: Int,
 ) {
     //<editor-fold desc="Genres">
     Action(1),
+    AdultCast(50, GenreType.THEME),  // Theme
     Adventure(2),
+    Anthropomorphic(51, GenreType.THEME),  // Theme
     AvantGarde(5),
     AwardWinning(46),
     BoysLove(28),
-    Cars(3, GenreType.THEME),  // Theme
+    CGDCT(52, GenreType.THEME),  // Theme
+    Childcare(53, GenreType.THEME),  // Theme
+    CombatSports(54, GenreType.THEME),  // Theme
     Comedy(4),
-    Demons(6, GenreType.THEME),  // Theme
+    Crossdressing(81, GenreType.THEME),  // Theme
+    Delinquents(55, GenreType.THEME),  // Theme
+    Detective(39, GenreType.THEME),  // Theme
     Drama(8),
     Ecchi(9, explicit = true),
+    Educational(56, GenreType.THEME),  // Theme
     Erotica(49, explicit = true),
     Fantasy(10),
-    Game(11, GenreType.THEME),  // Theme
+    GagHumor(57, GenreType.THEME),  // Theme
     GirlsLove(26),
+    Gore(58, GenreType.THEME),  // Theme
     Gourmet(47),
     Harem(35, GenreType.THEME),  // Theme
     Hentai(12, explicit = true),
+    HighStakesGame(59, GenreType.THEME),  // Theme
     Historical(13, GenreType.THEME),  // Theme
     Horror(14),
+    IdolsFemale(60, GenreType.THEME),  // Theme
+    IdolsMale(61, GenreType.THEME),  // Theme
+    Isekai(62, GenreType.THEME),  // Theme
+    Iyashikei(63, GenreType.THEME),  // Theme
     Josei(43, GenreType.DEMOGRAPHIC),  // Demographics
     Kids(15, GenreType.DEMOGRAPHIC),  // Demographics
-    Magic(16),
-    MartialArts(17),
+    LovePolygon(64, GenreType.THEME),  // Theme
+    MagicalSexShift(65, GenreType.THEME),  // Theme
+    MahouShoujo(66, GenreType.THEME),  // Theme
+    MartialArts(17, GenreType.THEME),  // Theme
     Mecha(18, GenreType.THEME),  // Theme
+    Medical(67, GenreType.THEME),  // Theme
     Military(38, GenreType.THEME),  // Theme
     Music(19, GenreType.THEME),  // Theme
     Mystery(7),
+    Mythology(6, GenreType.THEME),  // Theme
+    OrganizedCrime(68, GenreType.THEME),  // Theme
+    OtakuCulture(69, GenreType.THEME),  // Theme
     Parody(20, GenreType.THEME),  // Theme
-    Police(39, GenreType.THEME),  // Theme
+    PerformingArts(70, GenreType.THEME),  // Theme
+    Pets(71, GenreType.THEME),  // Theme
     Psychological(40, GenreType.THEME),  // Theme
+    Racing(3, GenreType.THEME),  // Theme
+    Reincarnation(72, GenreType.THEME),  // Theme
+    ReverseHarem(73, GenreType.THEME),  // Theme
     Romance(22),
+    RomanticSubtext(74, GenreType.THEME),  // Theme
     Samurai(21, GenreType.THEME),  // Theme
     School(23, GenreType.THEME),  // Theme
     SciFi(24),
     Seinen(42, GenreType.DEMOGRAPHIC),  // Demographics
     Shoujo(25, GenreType.DEMOGRAPHIC),  // Demographics
     Shounen(27, GenreType.DEMOGRAPHIC),  // Demographics
+    Showbiz(75, GenreType.THEME),  // Theme
     SliceOfLife(36),
     Space(29, GenreType.THEME),  // Theme
     Sports(30),
+    StrategyGame(11, GenreType.THEME),  // Theme
     SuperPower(31, GenreType.THEME),  // Theme
     Supernatural(37),
+    Survival(76, GenreType.THEME),  // Theme
     Suspense(41),
+    TeamSports(77, GenreType.THEME),  // Theme
+    TimeTravel(78, GenreType.THEME),  // Theme
     Vampire(32, GenreType.THEME),  // Theme
-    WorkLife(48),
+    VideoGame(79, GenreType.THEME),  // Theme
+    VisualArts(80, GenreType.THEME),  // Theme
+    Workplace(48, GenreType.THEME),  // Theme
     ;
     //</editor-fold>
 
